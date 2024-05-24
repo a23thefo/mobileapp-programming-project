7d3b67c553e2f467f57ad42439007ff6c6664415
Nixeff <theodore.folkesson@gmail.com>
2024-05-24 02:20:41 +0200

    Imported mainactivity from previous assignemnt

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
+    private final String JSON_FILE = "mountains.json";
+
+    ArrayList<Mountain> mountains= new ArrayList<>();
+
+    Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
+
     }
 
+
+
+    @Override
+    public void onPostExecute(String json) {
+        Type type = new TypeToken<ArrayList<Mountain>>() {}.getType();
+        ArrayList<Mountain> listOfMountains = gson.fromJson(json, type);
+
+        RecyclerView view = findViewById(R.id.recyclerView);
+        recyclerviewadapter adapter = new recyclerviewadapter (this, listOfMountains);
+        view.setAdapter(adapter);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+
+    }
 }
