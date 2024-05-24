ec603caf1a94852595a150ef7bc04bd491a76aad
Nixeff <theodore.folkesson@gmail.com>
2024-05-24 03:36:41 +0200

    Ladde till intents för att ta sig emellan MainActivity och SecondActivity. Skapade också texten i SecondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,12 +32,19 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonTask(this).execute(JSON_URL);
-
+        mainAc2();
     }
 
-    public void onButtonClickIntent(View button){
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        startActivity(intent);
+    private void mainAc2(){
+        Button click = (Button) findViewById(R.id.button);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("msg", "Hej på dig!");
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,5 +1,8 @@
 package com.example.project;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,18 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_activity);
 
         Bundle extras = getIntent().getExtras();
+        mainAc();
+    }
 
+    private void mainAc(){
+        Button click = (Button) findViewById(R.id.button2);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                intent.putExtra("msg", "Hej på dig!");
+                startActivity(intent);
+            }
+        });
     }
 }
